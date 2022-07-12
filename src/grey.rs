32bb0bc46d82d1b455d9a7445295d7d2d7ac1952
dcts <thomas@liist.com>
2022-07-12 19:45:48 +0200

    :sparkle: save

--- src/grey.rs
+++ src/grey.rs
@@ -1,24 +1,24 @@
 use battle_bots_engine::*;
 
-/** 
+/**
  * The grey bot is broken! It's using all the functions below, but they seem not to be implemented correctly
- * 
+ *
  * Please help us fix the bot!
- * 
+ *
  * Instructions
  * =============
- * 
+ *
  * Implement all the functions below
  * Run a battle (`cargo run`) after they have been implemented to test that the grey bot works again
  */
 
 /*
-// GRID POSITION (x,y) DERIVED FROM 
+// GRID POSITION (x,y) DERIVED FROM
 // https://github.com/holochain-immersive/battle-bots-engine/blob/1cf09bf8fd34958e31a084d08e902b7ed9665684/src/direction.rs#L20
 // Direction::Up if y + 1 < MAP_HEIGHT => (x, y + 1),
 // Direction::Down if (y as isize) - 1 >= 0 => (x, y - 1),
 // Direction::Right if x + 1 < MAP_WIDTH => (x + 1, y),
-// Direction::Left if (x as isize) - 1 >= 0 => (x - 1, y), 
+// Direction::Left if (x as isize) - 1 >= 0 => (x - 1, y),
 
 // CONCLUSION
 // UP    => y + 1
@@ -40,22 +40,23 @@ pub fn adjacent_position_in_direction(x: usize, y: usize, direction: Direction) 
 
 // Returns whether there is a bot in the given position
 pub fn is_bot(game_state: &GameState, position: &Position) -> bool {
-    let result = game_state.bots.iter().map(|tuple| {
-        let (position, bot) = tuple;
-        println!("{}", &bot);
-        return tuple;
-    });
-    return false;
-    // any(|&x| {
-        
-    // })
+    game_state
+        .bots
+        .iter()
+        .any(|(bot_position, _)| bot_position.x == position.x && bot_position.y == position.y )
 }
 
 // Returns the shortest way to rotate the "from" direction to get the "to" direction
 // Assumes that from and to are not equal
 // eg. shortest_rotation(Direction::Up, Direction::Right) == Rotation::Clockwise
 pub fn shortest_rotation(from: &Direction, to: &Direction) -> Rotation {
-    Rotation::Clockwise
+    match (from, to) {
+        (Direction::Up, Direction::Right) => Rotation::Clockwise,
+        (Direction::Right, Direction::Down) => Rotation::Clockwise,
+        (Direction::Down, Direction::Left) => Rotation::Clockwise,
+        (Direction::Left, Direction::Up) => Rotation::Clockwise,
+        _ => Rotation::Counterclockwise
+    }
 }
 
 // Rotate the given direction with the given rotation
@@ -63,10 +64,6 @@ pub fn shortest_rotation(from: &Direction, to: &Direction) -> Rotation {
 pub fn rotate_direction(direction: &Direction, rotation: &Rotation) -> Direction {
     Direction::Down
 }
-
-
-
-
 
 // ADD UNIT TESTS
 #[cfg(test)]
@@ -78,18 +75,27 @@ mod tests {
         // UP    => y + 1
         assert_eq!(adjacent_position_in_direction(4, 5, Direction::Up), (4, 6));
         // DOWN  => y - 1
-        assert_eq!(adjacent_position_in_direction(4, 5, Direction::Down), (4, 4));
+        assert_eq!(
+            adjacent_position_in_direction(4, 5, Direction::Down),
+            (4, 4)
+        );
         // RIGHT => x + 1
-        assert_eq!(adjacent_position_in_direction(4, 5, Direction::Right), (5, 5));
+        assert_eq!(
+            adjacent_position_in_direction(4, 5, Direction::Right),
+            (5, 5)
+        );
         // LEFT  => x - 1
-        assert_eq!(adjacent_position_in_direction(4, 5, Direction::Left), (3, 5));
+        assert_eq!(
+            adjacent_position_in_direction(4, 5, Direction::Left),
+            (3, 5)
+        );
     }
 
     // #[test]
     // fn is_bot_test() {
     //     // TODO
     // }
-    
+
     #[test]
     fn shortest_rotation_test() {
         // TODO => make test_shortcut_clockwise and test_shortcut_counterclockwise DRY
@@ -100,10 +106,10 @@ mod tests {
             match actual {
                 Rotation::Clockwise => {
                     success = true;
-                },
+                }
                 _ => {
                     success = false;
-                },
+                }
             }
             assert_eq!(success, true);
         }
@@ -114,10 +120,10 @@ mod tests {
             match actual {
                 Rotation::Counterclockwise => {
                     success = true;
-                },
+                }
                 _ => {
                     success = false;
-                },
+                }
             }
             assert_eq!(success, true);
         }
@@ -130,20 +136,20 @@ mod tests {
         test_shortcut_counterclockwise(&Direction::Left, &Direction::Down);
         test_shortcut_counterclockwise(&Direction::Down, &Direction::Right);
     }
-    
+
     #[test]
     fn rotate_direction_test() {
-        // TODO => make this terrible code DRY! 
+        // TODO => make this terrible code DRY!
         let actual: Direction = rotate_direction(&Direction::Up, &Rotation::Clockwise);
         let mut success: bool = false;
         println!("success = {}", success); // remove to see warning. Keep to avoid warning...
         match actual {
             Direction::Right => {
                 success = true;
-            },
+            }
             _ => {
                 success = false;
-            },
+            }
         }
         assert_eq!(success, true);
 
@@ -151,10 +157,10 @@ mod tests {
         match actual {
             Direction::Left => {
                 success = true;
-            },
+            }
             _ => {
                 success = false;
-            },
+            }
         }
         assert_eq!(success, true);
     }
